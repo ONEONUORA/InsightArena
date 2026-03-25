f2545ffae64841749d61e25b424001ca4deec638
ezekiel <favourtobiloba200@gmail.com>
2026-03-25 13:37:49 +0100

    Remove duplicate landing sections

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -76,8 +76,6 @@ export default function Home() {
           <ComparisonSection />
           <TransparentGrid />
           <StatisticsSection />
-          <TransparentGrid />
-          <StatisticsSection />
           <Faq />
           <Footer />
         </div>
